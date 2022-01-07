8bb76530287f2356e5291b9b4e97a674652906f6
jarrod-grayson <52757778+jarrod-grayson@users.noreply.github.com>
2022-01-07 17:10:53 -0500

    Creating PowerShell Script for Tosca API Results

---
+++ UploadAPIResultsToQtest.ps1
@@ -0,0 +1,14 @@
+$url = 'https://pulse-7.qtestnet.com/webhook/ea99f7b9-7a0a-4dcd-b99d-1a11e7c4c8e3'
+
+$content = Get-Content "C:\Tosca_Projects\ToscaCIResults\Results.xml" -Raw
+$bytes = [System.Text.Encoding]::ASCII.GetBytes($content)
+$payload = [System.Convert]::ToBase64String($bytes)
+
+$body = @{
+ 'projectId' = '121052'
+ 'testcycle' = '2960863'
+ 'result' = $payload
+}
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Invoke-RestMethod -Body $body -Method 'Post' -Uri $url
